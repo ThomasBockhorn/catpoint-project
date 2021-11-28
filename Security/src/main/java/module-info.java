80c20924203647ff839a4a2ef82a7e6cb18c8227
thomas <historian.thomas@gmail.com>
2021-11-28 17:50:53 -0600

    Created a new file with two modules

---
+++ Security/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.catpoint.security {
+    requires com.udacity.catpoint.image;
+    requires miglayout;
+    requires java.desktop;
+    requires guava;
+    requires gson;
+    requires java.prefs;
+
+}
