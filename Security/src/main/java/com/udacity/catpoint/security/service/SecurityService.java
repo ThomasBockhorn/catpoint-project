83c9c555951481a404b5c42099873301fcd1c6fc
thomas <historian.thomas@gmail.com>
2021-12-25 16:36:16 -0600

    changed the changeSensorActivationStatus method

--- Security/src/main/java/com/udacity/catpoint/security/service/SecurityService.java
+++ Security/src/main/java/com/udacity/catpoint/security/service/SecurityService.java
@@ -140,14 +140,15 @@ public class SecurityService {
      * @param active
      */
     public void changeSensorActivationStatus(Sensor sensor, Boolean active) {
-        if(!sensor.getActive() && active) {
-            handleSensorActivated();
-        } else if (sensor.getActive() && !active) {
-            handleSensorDeactivated();
-        } else if (sensor.getActive() || active){
-            handleSensorDeactivated();
-        }
+        AlarmStatus alarmStatus = securityRepository.getAlarmStatus();
 
+        if (alarmStatus != AlarmStatus.ALARM) {
+            if (active) {
+                handleSensorActivated();
+            } else if (sensor.getActive()) {
+                handleSensorDeactivated();
+            }
+        }
         sensor.setActive(active);
         securityRepository.updateSensor(sensor);
     }
